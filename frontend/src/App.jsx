3d5d0808fed4f516296aa8182775a3025d751a19
TirthDadhaniya <private.tirth@gmail.com>
2026-01-30 16:53:33 +0530

    Import and component error fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Auth-pages/Login.jsx";
+import Register from "./pages/Auth-pages/Register.jsx";
+import List from "./pages/List/List.jsx";
 import Employee from "./pages/Employee/Employee.jsx";
 import Designation from "./pages/Designation/Designation.jsx";
-import List from "./pages/List/List.jsx";
-import Login from "./pages/Login/Login.jsx";
-import Register from "./pages/Register/Register.jsx";
 import Salary from "./pages/Salary/Salary.jsx";
+import "./App.css";
 
 function App() {
   return (
@@ -15,10 +15,11 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/employee" element={<Employee />} />
-          <Route path="/designation" element={<Designation />} />
           <Route path="/list" element={<List />} />
+          <Route path="/designation" element={<Designation />} />
+          <Route path="/employee" element={<Employee />} />
           <Route path="/salary" element={<Salary />} />
+
           <Route path="*" element={<Login />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 createRoot(document.getElementById("root")).render(<App />);
